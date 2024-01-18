f12353b4aa0de33998f3832162488bf6aa56963a
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-18 19:44:46 +0000

    init

---
+++ 0x00-MySQL_Advanced/4-init.sql
@@ -0,0 +1,13 @@
+-- Initial
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS orders;
+
+CREATE TABLE IF NOT EXISTS items (
+    name VARCHAR(255) NOT NULL,
+    quantity int NOT NULL DEFAULT 10
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    item_name VARCHAR(255) NOT NULL,
+    number int NOT NULL
+);
