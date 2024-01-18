8e5c210461896fafe91db44fc22261d77a275b99
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-18 19:54:31 +0000

    all

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,11 @@
+DROP TRIGGER IF EXISTS reduce_quantity;
+DELIMITER $$
+CREATE TRIGGER reduce_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
