bfd580cbce98e14474c2af886a29f1e82b7d1fc8
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-19 18:12:25 +0000

    bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,3 +1,5 @@
+-- this creates a stored procedure AddBonus that adds a new
+-- correction for a student.
 DROP PROCEDURE IF EXISTS AddBonus;
 DELIMITER $$
 CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
