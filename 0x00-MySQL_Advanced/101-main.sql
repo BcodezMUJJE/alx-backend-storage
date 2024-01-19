d00c2000732aa8aa91974a0cfdb1733c2d90dca6
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-19 10:48:05 +0000

    101-main

---
+++ 0x00-MySQL_Advanced/101-main.sql
@@ -0,0 +1,11 @@
+USE holberton;
+
+-- Show and compute average weighted score
+SELECT * FROM users;
+SELECT * FROM projects;
+SELECT * FROM corrections;
+
+CALL ComputeAverageWeightedScoreForUsers();
+
+SELECT "--";
+SELECT * FROM users;
