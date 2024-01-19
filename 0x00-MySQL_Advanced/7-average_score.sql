f2eaf3e18bf28444128d396231be503868bb39dc
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-19 18:16:29 +0000

    average score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,5 +1,6 @@
+-- this creates a stored procedure ComputeAverageScoreForUser that
+-- computes and stores the average score for a student.
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
 BEGIN
