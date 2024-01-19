e56f950541d341c9f6bb977bd62dd0d30c91e74c
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-19 07:46:52 +0000

    store

---
+++ 0x00-MySQL_Advanced/4-main.sql
@@ -0,0 +1,11 @@
+USE holberton;
+
+SELECT * FROM items;
+SELECT * FROM orders;
+
+INSERT INTO orders (item_name, number) VALUES ('apple', 1);
+INSERT INTO orders (item_name, number) VALUES ('apple', 3);
+INSERT INTO orders (item_name, number) VALUES ('pear', 2);
+
+SELECT * FROM items;
+SELECT * FROM orders;
