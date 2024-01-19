0d3b8965bb3738f0aca52f0a425f7d2272f306dc
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-19 17:43:07 +0000

    0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
+-- this creates a table with unique users.
 DROP TABLE IF EXISTS users;
-
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
