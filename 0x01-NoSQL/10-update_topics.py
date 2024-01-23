2b5259d201d003b06684c440822fdb27055b2600
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-23 01:44:44 -0800

    main_4

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+'''Task 10: Change school topics
+'''
+
+
+def update_topics(mongo_collection, name, topics):
+    '''Changes all topics of a collection's document based on the name.
+    '''
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
