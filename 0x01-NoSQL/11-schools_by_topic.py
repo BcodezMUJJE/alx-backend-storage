74c13888cabce92913bd5aff88efa4ec500a5a5e
BcodezMUJJE <billbrightmatege@gmail.com>
2024-01-23 01:52:12 -0800

    main_6

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+'''Task 11: Where can I learn Python?
+'''
+
+
+def schools_by_topic(mongo_collection, topic):
+    '''Returns the list of school having a specific topic.
+    '''
+    topic_filter = {
+        'topics': {
+            '$elemMatch': {
+                '$eq': topic,
+            },
+        },
+    }
+    return [doc for doc in mongo_collection.find(topic_filter)]
